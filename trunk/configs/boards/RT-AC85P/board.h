0f4c405c780edf20990ab1c6423eab0ec4fc3a9e
fightroad <39027157+fightroad@users.noreply.github.com>
2024-08-30 09:12:34 +0800

    Add files via upload

---
+++ trunk/configs/boards/RT-AC85P/board.h
@@ -0,0 +1,21 @@
+/* RT-AC85P */
+
+#define BOARD_PID	"RT-AC85P"
+#define BOARD_NAME	"RT-AC85P"
+#define BOARD_DESC	"ASUS RT-AC85P Wireless Router"
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
+#define BOARD_VENDOR_URL	"https://www.asus.com.cn/"
+#define BOARD_MODEL_URL	"https://www.asus.com.cn/Networking-IoT-Servers/WiFi-Routers/ASUS-WiFi-Routers/RT-AC85P/"
+#define BOARD_BOOT_TIME	30
+#define BOARD_FLASH_TIME	120
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX  4
+#define BOARD_NUM_ANT_5G_RX  4
+#define BOARD_NUM_ANT_2G_TX  4
+#define BOARD_NUM_ANT_2G_RX  4
+#define BOARD_NUM_ETH_LEDS	5
+#define BOARD_NUM_ETH_EPHY	5
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	0
