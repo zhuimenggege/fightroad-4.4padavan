e21c4e85b92bd8ee163d720c396770075ebaff98
ztx <30760655@qq.com>
2024-10-01 19:16:48 +0800

    update

--- trunk/user/scripts/files/sbin/reset_ss.sh
+++ trunk/user/scripts/files/sbin/reset_ss.sh
@@ -55,36 +55,6 @@ echo 120 > /proc/sys/net/netfilter/nf_conntrack_udp_timeout_stream
 echo 30 > /proc/sys/net/netfilter/nf_conntrack_icmp_timeout
 echo 600 > /proc/sys/net/netfilter/nf_conntrack_generic_timeout
 
-# CPU IRQ
-set_rps_rfs() {
-    echo f >/proc/irq/11/smp_affinity
-    echo f >/proc/irq/12/smp_affinity
-    for device in $(ls /sys/class/net); do
-        echo f >/sys/class/net/$device/queues/rx-0/rps_cpus
-        echo 32768 >/sys/class/net/$device/queues/rx-0/rps_flow_cnt
-    done
-    echo 32768 >/proc/sys/net/core/rps_sock_flow_entries
-}
-get_rps_rfs() {
-    cat /proc/irq/11/smp_affinity
-    cat /proc/irq/12/smp_affinity
-    for device in $(ls /sys/class/net); do
-        printf "%-10s %-5s %-10s\n" "$device" "$(cat /sys/class/net/$device/queues/rx-0/rps_cpus)" "$(cat /sys/class/net/$device/queues/rx-0/rps_flow_cnt)"
-    done
-    cat /proc/sys/net/core/rps_sock_flow_entries
-}
-case $1 in
-get)
-    get_rps_rfs
-    ;;
-set)
-    set_rps_rfs
-    ;;
-*)
-    get_rps_rfs
-    ;;
-esac
-
 # panic
 echo 1        > /proc/sys/kernel/panic
 echo 1        > /proc/sys/kernel/panic_on_oops
